7d9c1ade98e23f37827580c45c02d41fb7b6dc2f
jeshwanthshivasai <havingthesensesofaspider@gmail.com>
2024-04-18 16:51:17 +0530

    change

--- learn string manipulation by building a CIPHER.py
+++ learn string manipulation by building a CIPHER.py
@@ -1,12 +1,16 @@
-text='Hello World'
-shift = 3
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-encrypted_text=''
-for char in text.lower():
-    if char==' ':
-        encrypted_text+=char
+text='Hello Zaira'
+custom_key='python'
+def vignere(message, key):
+    key_index=0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text=''
+    for char in message.lower():
+        #Append space to the message
+        if char==' ':
+            encrypted_text+=char
     else:
         index=alphabet.find(char)
-        new_index=index+shift
+        new_index=(index+offset)%len(alphabet)
         encrypted_text += alphabet[new_index]
-print('char:', char, 'encrypted_text:', encrypted_text)
+    print('plain text:', message)
+    print('encrypted_text:', encrypted_text)
