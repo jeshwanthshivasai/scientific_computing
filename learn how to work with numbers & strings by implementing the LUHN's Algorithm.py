8fb4d1d803d9e32863c5d85bafef3ddc72aa4cbb
jeshwanthshivasai <havingthesensesofaspider@gmail.com>
2024-04-24 20:35:09 +0530

    change

--- learn how to work with numbers & strings by implementing the LUHN's Algorithm.py
+++ learn how to work with numbers & strings by implementing the LUHN's Algorithm.py
@@ -15,10 +15,14 @@ def verify_card_number(card_number):
             sum_of_even_digits+=number
     total=sum_of_odd_digits + sum_of_even_digits
     print(total)
+    return 0 == total % 10
 def main():
-    card_number = '4111-1111-4555-1142'
+    card_number = '4111-1111-4555-1141'
     card_translation = str.maketrans({'-': '', ' ':''})
     translated_card_number = card_number.translate(card_translation)
-    print(translated_card_number)
-    verify_card_number(translated_card_number)
+
+    if verify_card_number(translated_card_number):
+        print('VALID!')
+    else:
+        print('INVALID!')
 main()
