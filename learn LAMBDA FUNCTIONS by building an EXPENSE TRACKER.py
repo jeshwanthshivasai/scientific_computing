196433b71bfaa033d1630edb3d7800894ceb2a44
jeshwanthshivasai <havingthesensesofaspider@gmail.com>
2024-04-25 21:59:36 +0530

    change

---
+++ learn LAMBDA FUNCTIONS by building an EXPENSE TRACKER.py
@@ -0,0 +1,10 @@
+def add_expense(expenses, amount, category):
+    expenses.append({'amount': amount, 'category': category})
+def print_expense(expenses):
+    for expense in expenses:
+        print(f"Amount: {expense["amount"]}, Category: {expense["category"]}")
+def total_expenses(expenses):
+    pass
+test=lambda x: x*2
+print(sum(map(test, [2,3, 5, 8])))        
+expenses=[]
