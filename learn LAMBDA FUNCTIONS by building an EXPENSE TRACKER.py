d2c1720cbdbd06693f5de2eaff1b7fb3020c0c50
jeshwanthshivasai <havingthesensesofaspider@gmail.com>
2024-05-04 17:38:38 +0530

    change

--- learn LAMBDA FUNCTIONS by building an EXPENSE TRACKER.py
+++ learn LAMBDA FUNCTIONS by building an EXPENSE TRACKER.py
@@ -2,7 +2,7 @@ def add_expense(expenses, amount, category):
     expenses.append({'amount': amount, 'category': category})
 def print_expenses(expenses):
     for expense in expenses:
-        print(f"Amount: {expense["amount"]}, Category: {expense["category"]}")
+        print(f'Amount: {expense["amount"]}, Category: {expense["category"]}')
 def total_expenses(expenses):
     sum(map(lambda expense: expense["amount"], expenses))
     return sum(map(lambda expense: expense["amount"], expenses))
@@ -25,3 +25,15 @@ def main():
         elif choice == '2':
             print('\nAll Expenses: ')
             print_expenses(expenses)
+        elif choice == '3':
+            print('\nTotal Expenses: ', total_expenses(expenses))
+        elif choice == '4':
+            category=input('Enter category to filter: ')
+            print(f'\n Expenses for {category}:')
+            expenses_from_category = filter_expenses_by_category(expenses, category)
+            print_expenses(expenses_from_category)
+        elif choice == '5':
+            print('Exiting the program')
+            break
+if __name__ == '__main__':
+    main()
